a422592e4032b2efdc0101ef70e020ee620ae5b3
Stephan Gahima <stephan.gahima@upc.edu>
2023-12-31 10:44:20 +0100

    Version 2
    - New layout
    - New color scheme

---
+++ static/js/src.js
@@ -0,0 +1,8 @@
+/*
+ * src.css
+ * Javascript for the website
+ * Stephan Gahima
+*/
+
+// Update the copyright year in the footer          
+document.getElementsByClassName("footer-copyright")[0].innerHTML = "&copy; Stephan Gahima " + new Date().getFullYear() + ". All rights reserved.";
