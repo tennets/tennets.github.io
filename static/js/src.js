79653bea9b2cf4a9a45f831d8e4f727fe0000e9a
Stephan Gahima <stephan.gahima@upc.edu>
2024-01-01 14:51:25 +0100

    First attempt to make the code modular

--- static/js/src.js
+++ static/js/src.js
@@ -4,5 +4,30 @@
  * Stephan Gahima
 */
 
+
+// Path to templates
+const TEMPLATES_PATH = "../../templates/";
+
+
+/*
+ * Inject the code of an HTML file into a specified element
+ *
+ * @param {string} file - The path to the HTML file
+ * @param {string} element - The element to inject the HTML into
+ */
+function injectHTML(file, element) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200)
+            document.getElementById(element).innerHTML = this.responseText;
+    };
+    xhttp.open("GET", file, true);
+    xhttp.send();
+}
+
+
+// Inject HTML code into the footer
+injectHTML(TEMPLATES_PATH + "footer.html", "footer");
+
 // Update the copyright year in the footer          
-document.getElementsByClassName("footer-copyright")[0].innerHTML = "&copy; Stephan Gahima " + new Date().getFullYear() + ". All rights reserved.";
+document.getElementsByClassName("footer-copyright")[0].innerHTML = "&copy; Stephan Gahima 2023 - " + new Date().getFullYear() + ". All rights reserved.";
